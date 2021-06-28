8d3f244c0270cc853da62a053456f813833f1f5f
Martin Jacquet <martin.jacquet@laas.fr>
2021-06-28 02:48:16 +0200

    handle compressed frames

--- codels/camviz_main_codels.cc
+++ codels/camviz_main_codels.cc
@@ -105,24 +105,35 @@ camera_main(uint16_t cam_id, const char prefix[64], float ratio,
     }
 
     camviz_camera_s* cam = &cameras->_buffer[cam_id];
-    if (frame->read(cam->name, self) != genom_ok || !frame->data(cam->name, self)) return camviz_pause_main;
+    if (frame->read(cam->name, self) != genom_ok || !frame->data(cam->name, self) || frame->data(cam->name, self)->pixels._length == 0)
+        return camviz_pause_main;
 
     // Retrieve frame and transform to opencv format
     or_sensor_frame* fdata = frame->data(cam->name, self);
 
-    int type;
-    if      (fdata->bpp == 1) type = CV_8UC1;
-    else if (fdata->bpp == 2) type = CV_16UC1;
-    else if (fdata->bpp == 3) type = CV_8UC3;
-    else if (fdata->bpp == 4) type = CV_8UC4;
-    else return camviz_e_sys_error("invalid frame type", self);
+    Mat cvframe;
+    if (fdata->compressed)
+    {
+        std::vector<uint8_t> buf;
+        buf.assign(fdata->pixels._buffer, fdata->pixels._buffer + fdata->pixels._length);
+        imdecode(buf, IMREAD_UNCHANGED, &cvframe);
+    }
+    else
+    {
+        int type;
+        if      (fdata->bpp == 1) type = CV_8UC1;
+        else if (fdata->bpp == 2) type = CV_16UC1;
+        else if (fdata->bpp == 3) type = CV_8UC3;
+        else if (fdata->bpp == 4) type = CV_8UC4;
+        else return camviz_e_sys_error("invalid frame type", self);
 
-    Mat cvframe = Mat(
-        Size(fdata->width, fdata->height),
-        type,
-        fdata->pixels._buffer,
-        Mat::AUTO_STEP
-    );
+        cvframe = Mat(
+            Size(fdata->width, fdata->height),
+            type,
+            fdata->pixels._buffer,
+            Mat::AUTO_STEP
+        );
+    }
 
     if (fdata->bpp == 1) cvtColor(cvframe, cvframe, COLOR_GRAY2BGR);
     if (fdata->bpp == 3) cvtColor(cvframe, cvframe, COLOR_RGB2BGR);  // opencv de ses morts
