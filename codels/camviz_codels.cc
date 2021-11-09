e47fc12c57d393055b6be94cc02511fac65b608e
Martin Jacquet <martin.jacquet@laas.fr>
2021-11-09 17:06:30 +0100

    turn add_pixel into an activity and add a retry (after 1ms) if the camera is not detected ; this should solve the issue of add_camera_start still ongoing when calling add_pixel, thus remove all useless sleep or pause from launch scripts

--- codels/camviz_codels.cc
+++ codels/camviz_codels.cc
@@ -66,45 +66,6 @@ stop_rec(char prefix[64], sequence_camviz_camera_s *cameras,
 }
 
 
-/* --- Function add_pixel_display --------------------------------------- */
-
-/** Codel add_pixel of function add_pixel_display.
- *
- * Returns genom_ok.
- * Throws camviz_e_sys.
- */
-genom_event
-add_pixel(const char pixel_name[64], const char cam_name[64],
-          sequence_camviz_camera_s *cameras, const genom_context self)
-{
-    // Check that requested camera exists
-    uint16_t cam_id;
-    for(cam_id=0; cam_id<cameras->_length; cam_id++)
-        if (!strcmp(cameras->_buffer[cam_id].name, cam_name))
-            break;
-    if (cam_id == cameras->_length)
-            return camviz_e_sys_error("camera not found", self);
-    camviz_camera_s* cam = &cameras->_buffer[cam_id];
-
-    // Check that pixel is not already displayed for the camera
-    uint16_t pix_id;
-    for(pix_id=0; pix_id<cam->pixel_ports._length; pix_id++)
-        if (!strcmp(cam->pixel_ports._buffer[pix_id], pixel_name))
-            return camviz_e_sys_error("pixel already in display", self);
-    // Allocate sequence memory if need be
-    if (pix_id >= cam->pixel_ports._maximum)
-        if (genom_sequence_reserve(&cam->pixel_ports, pix_id + 1))
-            return camviz_e_sys_error("add_pixel impossible", self);
-    (cam->pixel_ports._length)++;
-
-    // Add pixel to list
-    strcpy(cam->pixel_ports._buffer[pix_id], pixel_name);
-
-    warnx("display new pixel for camera %s: %s", cam_name, pixel_name);
-    return genom_ok;
-}
-
-
 /* --- Function stop ---------------------------------------------------- */
 
 /** Codel stop of function stop.
